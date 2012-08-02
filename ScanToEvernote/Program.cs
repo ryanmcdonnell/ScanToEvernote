7a2190976105d5c317971de743671e9155bb68b4
Ryan McDonnell <ryan@binaryfactory.com>
2012-08-02 11:49:03 -0700

    Add progress bar, cleanup

--- ScanToEvernote/Program.cs
+++ ScanToEvernote/Program.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using WIA;
 using System.Security.Principal;
 using System.Security.Permissions;
+using System.Threading;
+using System.ComponentModel;
 
 namespace ScanToEvernote
 {
@@ -17,7 +19,7 @@ namespace ScanToEvernote
 		[STAThread]
 		static void Main(string[] args)
 		{
-			if (args.Length > 0 && args[0] == "regsiter")
+			if (args.Length > 0 && args[0] == "register")
 			{
 				try
 				{
@@ -31,12 +33,43 @@ namespace ScanToEvernote
 					MessageBox.Show(String.Format("Error registering as scanner button event: {0}\n\nAre you an administrator?", ex.Message), "ScanToEvernote", MessageBoxButtons.OK);
 				}
 
-				Application.Exit();
+				return;
 			}
 
 			if (String.IsNullOrEmpty(Properties.Settings.Default.EvernoteDeveloperToken))
 				Properties.Settings.Default.EvernoteDeveloperToken = Microsoft.VisualBasic.Interaction.InputBox("What is your Evernote API Token?", "ScanToEvernote", "");
 
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+
+			RunBackgroundWorker();
+
+			Application.Run(new ProgressForm());
+		}
+
+		private static void RunBackgroundWorker()
+		{
+			// this is your presumably long-running method
+			Action exec = ScanImagesAndUpload;
+
+			BackgroundWorker b = new BackgroundWorker();
+
+			// set the worker to call your long-running method
+			b.DoWork += (object sender, DoWorkEventArgs e) =>
+			{
+				exec.Invoke();
+			};
+
+			// this only tells your BackgroundWorker to START working;
+			// the current (i.e., GUI) thread will immediately continue,
+			// which means your progress bar will update, the window
+			// will continue firing button click events and all that
+			// good stuff
+			b.RunWorkerAsync();
+		}
+
+		private static void ScanImagesAndUpload()
+		{
 			try
 			{
 				Scanner scanner = new Scanner();
@@ -55,6 +88,8 @@ namespace ScanToEvernote
 			{
 				MessageBox.Show(ex.Message, "ScanToEvernote", MessageBoxButtons.OK);
 			}
+
+			Application.Exit();
 		}
 	}
 }
---
+++ ScanToEvernote/ProgressForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace ScanToEvernote
+{
+	partial class ProgressForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Location = new System.Drawing.Point(12, 29);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(268, 23);
+			this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+			this.progressBar1.TabIndex = 0;
+			this.progressBar1.Value = 1;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.BackColor = System.Drawing.Color.Transparent;
+			this.label1.Location = new System.Drawing.Point(13, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(189, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Scanning and uploading to Evernote...";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// ProgressForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(292, 68);
+			this.Controls.Add(this.progressBar1);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "ProgressForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "ProgressForm";
+			this.Load += new System.EventHandler(this.ProgressForm_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.Label label1;
+	}
+}
